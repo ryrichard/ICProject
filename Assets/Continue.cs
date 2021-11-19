5d2593eec8c2dce19890e7b47618c4bb0ca0d16c
Fredy Diaz <fredy.diaz@stu.bmcc.cuny.edu>
2021-11-19 13:47:08 -0500

    slides

--- Assets/Continue.cs
+++ Assets/Continue.cs
@@ -7,13 +7,6 @@ public class Continue : MonoBehaviour
     public LinearConveyor conveyor;
     private float speed = 0.3f;
 
-    void Start()
-    {
-
-    }
-
-
-
     public void OnClick(){
       Debug.Log("OnClick");
       if(conveyor.speed == 0.0f){
